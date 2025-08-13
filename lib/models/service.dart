149000750bfdf024d66e2cfe1f613b46450a7473
zehra-ozkan <zehra.ozkan138@gmail.com>
2025-08-13 11:43:51 +0300

    securing...

--- lib/models/service.dart
+++ lib/models/service.dart
@@ -1,36 +1,29 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ApiService {
-  //static const String baseUrl = "http://10.0.2.2:8080"; // Use the working URL
-  final possibleUrls = [
-    'http://10.0.2.2:8080', // Android emulator
-    'http://localhost:8080', // iOS simulator/desktop
-    'http://192.168.1.10:8080', // Replace with your computer's IP
-  ];
+  String baseUrl = dotenv.get('API_BASE_URL');
 
   Future<List<dynamic>> getDepartments() async {
-    for (var baseUrl in possibleUrls) {
-      final response = await http.get(Uri.parse('$baseUrl/departments'));
-      if (response.statusCode == 200) {
-        return jsonDecode(response.body);
-      }
+    final response = await http.get(Uri.parse('$baseUrl/departments'));
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
     }
     throw Exception('Failed to load departments');
   }
 
   Future<dynamic> createDepartment(String name) async {
-    for (var baseUrl in possibleUrls) {
-      final response = await http.post(
-        Uri.parse('$baseUrl/departments'),
-        headers: {'Content-Type': 'application/json'},
-        body: jsonEncode({'name': name}),
-      );
+    final response = await http.post(
+      Uri.parse('$baseUrl/departments'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'name': name}),
+    );
 
-      if (response.statusCode == 201) {
-        return jsonDecode(response.body);
-      }
+    if (response.statusCode == 201) {
+      return jsonDecode(response.body);
     }
+
     throw Exception('Failed to create department');
   }
 }
